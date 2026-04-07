c911e3d355eda9f7be39a057116301c3f10f92a0
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2026-04-07 16:14:04 +0530

    Update code.py

--- Hashing + Prefix Sum/49. Group Anagrams/code.py
+++ Hashing + Prefix Sum/49. Group Anagrams/code.py
@@ -4,12 +4,12 @@ class Solution:
         for s in strs:
             count = [0]*26
             for c in s:
-                """
-                [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0] (= "eat")
-                [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0] (= "tea")
-                """
                 count[ord(c) - ord('a')] += 1
+                """
+                    [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0] (= "eat")
+                    [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0] (= "tea")
+                """
             group[tuple(count)].append(s) # tuple is hashable, list is not; group all anagrams with the same key
 
-        return list(group.values())
+        return list(group.values()) # O(m*n), O(m*n)
         

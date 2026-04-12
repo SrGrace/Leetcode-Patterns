b3e70f5c6c085e82e6b1f8ad1ba75c64dfa217fc
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2026-04-12 20:22:56 +0530

    Create code.py

---
+++ Intervals/57. Insert Interval/code.py
@@ -0,0 +1,19 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x: x[0])
+  
+        merged = []  
+        prev = intervals[0] 
+        for interval in intervals[1:]:
+            if interval[0] <= prev[1]: # start <= end
+                prev[1] = max(prev[1], interval[1])
+            
+            else:
+                merged.append(prev)
+                prev = interval
+
+        merged.append(prev)
+            
+        return merged # O(nlogn), O(n)
+
